2da953dac562a3daceb776ad2e15655a0cc31a7d
eldad-assaf <eldadux@gmail.com>
2023-09-29 16:52:26 +0300

    TasksPage

--- lib/features/tasks/presentation/pages/tasks_page.dart
+++ lib/features/tasks/presentation/pages/tasks_page.dart
@@ -1,7 +1,6 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store_flutter_clean_code_nodejs/features/tasks/domain/entities/task_entity.dart';
 import 'package:store_flutter_clean_code_nodejs/features/tasks/presentation/bloc/tasks_bloc.dart';
 
 class TasksPage extends StatefulWidget {
@@ -14,14 +13,39 @@ class _TasksPage extends State<TasksPage> {
   Widget build(BuildContext context) {
     return BlocBuilder<TasksBloc, TasksState>(
       builder: (context, state) {
-        log(state.toString());
+        if (state is TasksLoading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (state is TasksError) {
+          return Center(
+            child: Text('Something went wrong'),
+          );
+        }
         if (state.tasks != null) {
+          final List<TaskEntity> tasks = state.tasks!;
+          ;
           return Container(
             child: ListView.builder(
               itemCount: state.tasks!.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                    title: Text(state.tasks![index].name.toString()));
+                final TaskEntity task = tasks[index];
+                if (task.name != null &&
+                    task.id != null &&
+                    task.completed != null) {
+                  return ListTile(
+                      title: Text(task.name!),
+                      subtitle:
+                          Text(task.completed.toString() + ' ' + task.id!),
+                      trailing: IconButton(
+                        onPressed: () {
+                          
+                        },
+                        icon: Icon(Icons.remove_circle),
+                      ));
+                }
+                return null;
               },
             ),
           );
